54480fd35425815b6127abce615540dd421069fb
Deven Phillips <deven.phillips@gmail.com>
2018-08-20 09:02:32 -0400

    Initial commit with Quasar

--- src/main.js
+++ src/main.js
@@ -3,11 +3,55 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import i18n from './i18n';
+
+import './styles/quasar.styl';
+import 'quasar-extras/material-icons';
+import Quasar, {
+  QBtn,
+  QLayout,
+  QLayoutHeader,
+  QLayoutDrawer,
+  QPage,
+  QPageContainer,
+  QToolbar,
+  QToolbarTitle,
+  QList,
+  QListHeader,
+  QItemSeparator,
+  QItem,
+  QItemSide,
+  QItemMain,
+} from 'quasar';
+
+Vue.use(Quasar, {
+  components: {
+    QBtn,
+    QLayout,
+    QLayoutHeader,
+    QLayoutDrawer,
+    QPage,
+    QPageContainer,
+    QToolbar,
+    QToolbarTitle,
+    QList,
+    QListHeader,
+    QItemSeparator,
+    QItem,
+    QItemSide,
+    QItemMain,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
